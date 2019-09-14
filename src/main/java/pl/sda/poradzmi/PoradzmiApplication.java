6fbbdcda5e9cc367dca931cc164a2b11162b1cb7
Marek Michalski <nydmif@gmail.com>
2019-09-14 12:16:04 +0200

    Initial commit

---
+++ src/main/java/pl/sda/poradzmi/PoradzmiApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.poradzmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoradzmiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoradzmiApplication.class, args);
+    }
+
+}
